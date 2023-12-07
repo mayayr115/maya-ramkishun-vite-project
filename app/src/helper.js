76da1853d5bd7e26842578e14d9878a48cf9e04f
mayayr115 <mayaramkishuncsh2027@gmail.com>
2023-12-06 22:32:25 -0500

    Wednesday Progress

---
+++ app/src/helper.js
@@ -0,0 +1,27 @@
+const setLocalStorageKey = (key, value) => {
+
+}
+
+const getLocalStorageKey = (key) => {
+  
+}
+
+const getPalettes = () => {
+
+}
+
+const setPalettes = (newPalettes) => {
+
+}
+
+const initPalettesIfEmpty = () => {
+
+}
+
+const addPalette = (newPalette) => {
+
+}
+
+const removePalette = (paletteUuid) => {
+
+}
--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,28 @@
 import './style.css'
+import palettes from '../palettes.json' // JSON data
 
-document.querySelector('#app').innerHTML = '<h1>Hello World</h1>'
+console.log(palettes); // test
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the container element, aka div
+  const palettesContainer = document.getElementById("app");
+
+  // Create an unordered list to display color palettes
+  const ul = document.createElement("ul");
+
+  // Iterate through each color palette in the JSON data
+  palettes.forEach((palette) => {
+      // Create list items for each color palette
+      // Convert to just innerHTML?
+      const li = document.createElement("li");
+      li.innerHTML = `<strong>${palette.title}</strong><div class="color-swatch-container">${palette.colors.map(color => `<div class="color-swatch" style="background-color: ${color};"></div>`).join('')}</div>(Temperature: ${palette.temperature})`;
+
+      // Append each list item to the unordered list
+      ul.appendChild(li);
+  });
+
+  // Append the unordered list to the container
+  palettesContainer.appendChild(ul);
+});
+
+
